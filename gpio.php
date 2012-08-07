413a3ea475d4321e9b09ad4f6f39dab8075310fe
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2012-08-07 19:39:27 +0000

    Added PHP version for server side

---
+++ gpio.php
@@ -0,0 +1,99 @@
+<?php
+
+class GPIO {
+	var $GPIO_AVAILABLE = [0, 1, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 21, 22, 23, 24, 25];
+	var $ALT = [
+		"UART" =>["enabled" => true, "pins"=>[14, 15]],
+		"I2C" => ["enabled" => false, "pins"=>[0, 1]],
+		"SPI" => ["enabled" => false, "pins"=>[7, 8, 9, 10, 11]]
+	];
+	var $GPIO_ROOT = "/sys/class/gpio";
+
+	function setALT($alt, $enable) {
+		$alt["enabled"] = $enable;
+	}
+
+	function getPath($pin, $cmd="") {
+		return $this->GPIO_ROOT . "/gpio" . $pin . "/" . $cmd;
+	}
+
+	function isEnabled($pin) {
+		return file_exists($this->getPath($pin));
+	}
+
+	function enable($pin) {
+		system("sudo sh -c 'echo $pin > $this->GPIO_ROOT/export'");
+		return $this->isEnabled($pin);
+	}
+
+	function disable($pin) {
+		system("sudo sh -c 'echo $pin > $this->GPIO_ROOT/unexport'");
+		return $this->isEnabled($pin);
+	}
+
+	function readGPIO($pin, $file) {
+		$path = $this->getPath($pin, $file);
+		$f= fopen($path, "r");
+		$value = fread($f, filesize($path));
+		$value = substr($value, 0, strpos($value, "\n"));
+		fclose($f);
+		return $value;
+		
+	}
+	
+	function getDirection($pin) {
+		return $this->readGPIO($pin, "direction");
+	}
+
+	function setDirection($pin, $value) {
+		system("sudo sh -c 'echo $value > " . $this->getPath($pin, "direction") . "'");
+		return $this->getDirection($pin);
+	}
+
+	function getValue($pin) {
+		return $this->readGPIO($pin, "value");
+	}
+
+	function setValue($pin, $value) {
+		system("sudo sh -c 'echo $value > " . $this->getPath($pin, "value") . "'");
+		return $this->getValue($pin);
+	}
+
+	function getALTPins() {
+		$alt_pins = array();
+		foreach($this->ALT as $alt) {
+			if ($alt["enabled"]) {
+				foreach($alt["pins"] as $pin) {
+					$alt_pins[] = $pin;
+				}
+			}
+		}
+		return $alt_pins;
+	}
+
+	function setPINSEnabled($enable) {
+		$alt_pins = $this->getALTPins();
+		foreach($this->GPIO_AVAILABLE as $pin) {
+			if (!in_array($pin, $alt_pins)) {
+				if ($enable && !$this->isEnabled($pin)) {
+					echo "enabling " . $pin . "</br>";
+					$this->enable($pin);
+				}
+				else if (!$enable && $this->isEnabled($pin)) {
+					$this->disable($pin);
+				}
+			}
+		}
+
+	}
+
+	function init() {
+		$this->setPINSEnabled(true);
+	}
+
+	function release() {
+		$this->setPINSEnabled(false);
+	}
+}
+
+?>
---
+++ webiopi.php
@@ -0,0 +1,106 @@
+<?php
+
+include("gpio.php");
+
+function sendError($code, $message) {
+	header($_SERVER['SERVER_PROTOCOL'] . " " . $code . " " . $message);
+	exit($message);
+}
+
+function doGET($gpio, $vars) {
+	if ($vars[1] == "*") {
+		writeJSON($gpio);
+	}
+	else {
+		sendError(404, "Not Found");
+	}
+}
+
+function writeJSON($gpio) {
+	$pins = $gpio->GPIO_AVAILABLE;
+	$alt = $gpio->getALTPins();
+
+	header("Content-Type: application/json");
+	echo '{"UART": 1, "I2C": 0, "SPI": 0, "GPIO":{'. "\n";
+
+	$first = true;
+	foreach($pins as $pin) {
+		if (!$first)
+			print ", \n";
+
+		$mode = "GPIO";
+		$direction = "out";
+		$value = 0;
+		if (in_array($pin, $alt)) {
+			$mode = "ALT";
+		} 
+		else {
+			$direction = $gpio->getDirection($pin);
+			$value = $gpio->getValue($pin);
+		}
+		
+		printf ('"%d": {"mode": "%s", "direction": "%s", "value": %d}', $pin, $mode, $direction, $value);
+		$first = false;
+	}
+
+	echo "\n}}";
+}
+
+function doPOST($gpio, $vars) {
+	if ($vars[1] == "GPIO") {
+		if (count($vars) != 5) {
+			sendError(400, "Bad Request");
+		}
+		$pin = $vars[2];
+		$cmd = $vars[3];
+		$val = $vars[4];
+		$ret = "";
+
+		if ($cmd == "direction") {
+			$ret = $gpio->setDirection($pin, $val);
+		}
+		else if ($cmd == "value") {
+			$ret = $gpio->setValue($pin, $val);
+		}
+		else {
+			sendError(400, "Bad Request");
+		}
+
+		header("Content-Type: text/plain");
+		print $ret;
+	}
+	else {
+		sendError(404, "Not Found");
+	}
+}
+
+function routeRequest($gpio) {
+	$method = $_SERVER['REQUEST_METHOD'];
+	$request_uri = $_SERVER['REQUEST_URI'];
+	$root = $_SERVER['DOCUMENT_ROOT'];
+	$script = $_SERVER['SCRIPT_FILENAME'];
+
+	$path = pathinfo($script);
+	$context = substr($path['dirname'], strlen($root));
+	$uri = substr($request_uri, strlen($context));
+
+	$vars = explode('/', $uri);
+	if ($method == "GET") {
+		doGET($gpio, $vars);
+	}
+	else if ($method == "POST") {
+		doPOST($gpio, $vars);
+	}
+	else {
+		sendError(405, "Not Allowed");
+	}
+}
+
+
+$gpio = new GPIO();
+$gpio->init();
+
+routeRequest($gpio);
+
+
+?>
