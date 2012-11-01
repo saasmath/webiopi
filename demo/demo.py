c696917561668bca0f6776446ee98d8d117da46d
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2012-11-01 22:23:23 +0000

    cleanup

--- demo/demo.py
+++ demo/demo.py
@@ -6,22 +6,20 @@ def myFunction(data):
     print "myFunction (%s)" % data
     return "OK"
 
-if __name__ == "__main__":
-    server = webiopi.Server()
-    server.addFunction("myFunction", myFunction)
-    
-    gpio.setmode(gpio.BCM)
-    gpio.setup(7, gpio.OUT)
-    
-    try:
-        while True:
-            gpio.output(7, 1)
-            time.sleep(5)
-            gpio.output(7, 0)
-            time.sleep(5)
-    except KeyboardInterrupt:
-        pass
-    
-    server.stop()
-    gpio.setup(7, gpio.IN)
+server = webiopi.Server()
+server.addFunction("myFunction", myFunction)
 
+gpio.setmode(gpio.BCM)
+gpio.setup(7, gpio.OUT)
+
+try:
+    while True:
+        gpio.output(7, 1)
+        time.sleep(5)
+        gpio.output(7, 0)
+        time.sleep(5)
+except KeyboardInterrupt:
+    pass
+
+server.stop()
+gpio.setup(7, gpio.IN)
