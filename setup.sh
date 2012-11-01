4b9b2a6337d51240a61a57ffc8e5a4454b7fc8c6
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2012-11-01 22:07:33 +0000

    updated setup script

--- setup.sh
+++ setup.sh
@@ -6,3 +6,5 @@ rm -rf /usr/share/webiopi
 mkdir /usr/share/webiopi
 cp -v -r htdocs /usr/share/webiopi/htdocs
 cp -v -r doc /usr/share/webiopi/doc
+cp -v python/webiopi.py.init /etc/init.d/webiopi
+chmod a+x /etc/init.d/webiopi
