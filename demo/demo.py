cfec03fb3482e5a4d59dd3783e8cbdd9557ae286
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2012-11-04 23:35:20 +0000

    commented demo

--- demo/demo.py
+++ demo/demo.py
@@ -1,24 +1,35 @@
+#First import webiopi
 import webiopi
 import time
 
+#I use the integrated GPIO lib, but you can use RPi.GPIO
 GPIO = webiopi.GPIO
 
+#A custom macro which prints out the args sent and return OK
 def myMacro(data):
     print "myMacro (%s)" % data
     return "OK"
 
-server = webiopi.Server()
+#Instantiate the server on the port 8000, it starts immediately in its own thread
+server = webiopi.Server(port=8000, login="admin", password="p@ssw0rd")
+
+#Register the macro so you can call it through the [RESTAPI] or [JAVASCRIPT]
 server.addMacro("myMacro", myMacro)
 
+#Setup GPIO 0 and 7
 GPIO.setFunction(0, GPIO.IN)
 GPIO.setFunction(7, GPIO.OUT)
 
+#Example loop which toggle GPIO 7 each 5 seconds
 try:
     while True:
         GPIO.output(7, not GPIO.input(7))
         time.sleep(5)
+
+#Break the loop by pressing CTRL-C
 except KeyboardInterrupt:
     pass
 
+#Stops the server
 server.stop()
 GPIO.setFunction(7, GPIO.IN)
