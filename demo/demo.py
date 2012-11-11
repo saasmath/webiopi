451996cacebb90c382758e1592f1a318550f1605
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2012-11-11 17:01:45 +0000

    updated demo

--- demo/demo.py
+++ demo/demo.py
@@ -11,25 +11,21 @@ def myMacro(arg):
     return "OK"
 
 # Instantiate the server on the port 8000, it starts immediately in its own thread
-server = webiopi.Server(port=8000, login="demo", password="demo")
+server = webiopi.Server(port=8000, login="webiopi", password="raspberry")
 
 # Register the macro so you can call it through the [RESTAPI] or [JAVASCRIPT]
 server.addMacro(myMacro)
 
 # Setup GPIOs
-GPIO.setFunction(7, GPIO.IN)
-GPIO.setFunction(9, GPIO.OUT)
-GPIO.setFunction(10, GPIO.PWM)
-GPIO.setFunction(11, GPIO.PWM)
-
-# output PWM
-GPIO.pulseRatio(10, 0.25) # 25% duty cycle on GPIO 10 (float ratio from 0.0 to 1.0)
-GPIO.pulseAngle(11, 0)    # servo neutral on GPIO 11 (float angle from -45 to +45)
+GPIO.setFunction(1, GPIO.IN)
+GPIO.setFunction(7, GPIO.OUT)
+GPIO.setFunction(8, GPIO.PWM)
+GPIO.setFunction(9, GPIO.PWM)
 
 # Example loop which toggle GPIO 9 each 5 seconds
 try:
     while True:
-        GPIO.output(9, not GPIO.input(9))
+        GPIO.output(7, not GPIO.input(7))
         time.sleep(5)        
 
 # Break the loop by pressing CTRL-C
@@ -40,7 +36,7 @@ except KeyboardInterrupt:
 server.stop()
 
 # Reset GPIO functions
+GPIO.setFunction(1, GPIO.IN)
 GPIO.setFunction(7, GPIO.IN)
+GPIO.setFunction(8, GPIO.IN)
 GPIO.setFunction(9, GPIO.IN)
-GPIO.setFunction(10, GPIO.IN)
-GPIO.setFunction(11, GPIO.IN)
