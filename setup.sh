667c58f82953a98c311d81a15e9665b0a93bcf24
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2012-12-18 22:24:05 +0000

    improved setup script for PiStore

--- setup.sh
+++ setup.sh
@@ -14,8 +14,19 @@ for python in $SEARCH; do
 		version=`$python -V 2>&1`
 		include=`$python -c "import distutils.sysconfig; print(distutils.sysconfig.get_python_inc())"`
 		echo "Found $version... " 
+		
+		if [ ! -f "$include/Python.h" ]; then
+			echo "Trying to install $python-dev using apt-get"
+			apt-get install -y "$python-dev"
+		fi
+			
 		if [ -f "$include/Python.h" ]; then
-			$python setup.py install || (echo "Build for $version failed\n" && continue) 
+			echo "Trying to install WebIOPi for $version"
+			$python setup.py install
+			if [ "$?" -ne "0" ]; then
+				echo "Build for $version failed\n"
+				continue
+			fi 
 			echo "WebIOPi installed for $version\n"
 			INSTALLED="$INSTALLED $python"
 	    else
@@ -29,26 +40,28 @@ cd ..
 if [ -z "$INSTALLED" ]; then
 	if [ -z "$FOUND" ]; then
 		echo "ERROR: WebIOPi cannot be installed - neither python or python3 found"
+		exit 1
 	else
-		echo "ERROR: WebIOPi cannot be installed - please install python development package first"
+		echo "ERROR: WebIOPi cannot be installed - please check errors above"
+		exit 2
 	fi
-	exit
 fi
 
 for python in $INSTALLED; do
 	echo $python > /dev/null
 done 
 
-echo -n "Copying resources... " 
+echo "Copying resources..."
 mkdir /usr/share/webiopi 2>/dev/null 1>/dev/null
 cp -rf htdocs /usr/share/webiopi
 cp -rf python/webiopi.py.init /etc/init.d/webiopi
+echo
+echo "Setting up startup script..."
 chmod 0755 /etc/init.d/webiopi
 sed -i "s/python/$python/g" /etc/init.d/webiopi
-echo "OK\n"
-echo "WebIOPi successfully installed"
-
-for python in $INSTALLED; do
-	version=`$python -V 2>&1`
-	echo "* You can use it with $version\t: sudo $python -m webiopi"
-done
+update-rc.d webiopi defaults
+echo
+echo "Starting WebIOPi..."
+/etc/init.d/webiopi restart
+echo
+echo "WebIOPi successfully installed, open a browser to http://localhost:8000/"
