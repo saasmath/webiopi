f0688aa1313cd8200c254afed7c9e790fed1884d
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2012-12-27 23:21:06 +0000

    added Serial class

--- python/webiopi.py
+++ python/webiopi.py
@@ -47,6 +47,17 @@ MAPPING = [[], [], []]
 MAPPING[1] = ["V33", "V50", 0, "V50", 1, "GND", 4, 14, "GND", 15, 17, 18, 21, "GND", 22, 23, "V33", 24, 10, "GND", 9, 25, 11, 8, "GND", 7]
 MAPPING[2] = ["V33", "V50", 2, "V50", 3, "GND", 4, 14, "GND", 15, 17, 18, 27, "GND", 22, 23, "V33", 24, 10, "GND", 9, 25, 11, 8, "GND", 7]
     
+def runLoop(func=None):
+    try:
+        while True:
+            if func != None:
+                func()
+            else:
+                time.sleep(1)
+            
+    except KeyboardInterrupt:
+        pass
+
 def encodeAuth(login, password):
     abcd = "%s:%s" % (login, password)
     try:
@@ -428,19 +439,45 @@ class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
                 
         else: # path unknowns
             self.send_error(404, "Not Found")
-            
-def runLoop(func=None):
-    try:
-        while True:
-            if func != None:
-                func()
-            else:
-                time.sleep(1)
-            
-    except KeyboardInterrupt:
-        pass
-    
-            
+
+class Serial:
+    def __init__(self, baudrate=9600, port="/dev/ttyAMA0"):
+        aname = "B%d" % baudrate
+        if not hasattr(termios, aname):
+            raise Exception("Unsupported baudrate")
+        speed = getattr(termios, aname)
+
+        self.fd = os.open(port, os.O_RDWR | os.O_NOCTTY | os.O_NDELAY)
+        
+        if self.fd < 0:
+            raise Exception("Cannot open %s" % port)
+        
+        fcntl.fcntl(self.fd, fcntl.F_SETFL, os.O_NDELAY)
+        
+        backup  = termios.tcgetattr(self.fd)
+        options = termios.tcgetattr(self.fd)
+        
+        options[4] = speed # ispeed
+        options[5] = speed # ospeed
+        
+        # cflags
+        options[2] |= (termios.CLOCAL | termios.CREAD)
+        options[2] &= ~termios.PARENB
+        options[2] &= ~termios.CSTOPB
+        options[2] &= ~termios.CSIZE
+        options[2] |= termios.CS8
+        
+        termios.tcsetattr(self.fd, termios.TCSADRAIN, options)
+
+    def close(self):
+        os.close(self.fd)
+        
+    def write(self, string):
+        os.write(self.fd, string)
+        
+    def read(self):
+        return os.read(self.fd)
+
 def main(argv):
     port = 8000
     passwdfile = "/etc/webiopi/passwd"
