1355ffbdca17c1e1028f8862ec281a9713d51ccd
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-01-06 19:46:14 +0000

    removed experimental serial class

--- python/webiopi.py
+++ python/webiopi.py
@@ -468,44 +468,6 @@ class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_POST(self):
         self.processRequest()
 
-class Serial:
-    def __init__(self, baudrate=9600, port="/dev/ttyAMA0"):
-        aname = "B%d" % baudrate
-        if not hasattr(termios, aname):
-            raise Exception("Unsupported baudrate")
-        speed = getattr(termios, aname)
-
-        self.fd = os.open(port, os.O_RDWR | os.O_NOCTTY | os.O_NDELAY)
-        
-        if self.fd < 0:
-            raise Exception("Cannot open %s" % port)
-        
-        fcntl.fcntl(self.fd, fcntl.F_SETFL, os.O_NDELAY)
-        
-        backup  = termios.tcgetattr(self.fd)
-        options = termios.tcgetattr(self.fd)
-        
-        options[4] = speed # ispeed
-        options[5] = speed # ospeed
-        
-        # cflags
-        options[2] |= (termios.CLOCAL | termios.CREAD)
-        options[2] &= ~termios.PARENB
-        options[2] &= ~termios.CSTOPB
-        options[2] &= ~termios.CSIZE
-        options[2] |= termios.CS8
-        
-        termios.tcsetattr(self.fd, termios.TCSADRAIN, options)
-
-    def close(self):
-        os.close(self.fd)
-        
-    def write(self, string):
-        os.write(self.fd, string)
-
-    def read(self, bytecount=1):
-        return os.read(self.fd, bytecount)
-
 def main(argv):
     port = 8000
     passwdfile = "/etc/webiopi/passwd"
