83858c153f2fd887129a8c62a27cd98c3d905e5b
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-01-07 20:59:32 +0000

    improved play script to launch a new midori instance in appmode

--- play.sh
+++ play.sh
@@ -5,7 +5,7 @@
 sudo /etc/init.d/webiopi start
 
 # Launch the browser
-midori http://localhost:8000/
+midori -c `pwd`/midori -a http://localhost:8000/
 
 # Stop WebIOPi service
 sudo /etc/init.d/webiopi stop
