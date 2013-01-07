9b39043a92eb16d0961b0138124fad68f7acc968
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-01-07 21:47:05 +0000

    added version check to force update

--- play.sh
+++ play.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 # WebIOPi PiStore launch script
 
+VERSION=`python -c "import webiopi; print(webiopi.VERSION)"`
+if [ "$VERSION" != "0.5.3" ]; then
+	echo "Update required..."
+	sudo ./setup.sh
+fi
+
 # Start WebIOPi service
 sudo /etc/init.d/webiopi start
 
