af284bcce8bea38e0ebba5ea212ed387b0a2f131
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-01-07 00:25:08 +0000

    added macro usage examples

--- demo/demo.py
+++ demo/demo.py
@@ -1,27 +1,31 @@
-# First import webiopi
+# Imports
 import webiopi
 import time
 
-# I use the integrated GPIO lib, but you can use RPi.GPIO
+# Retrieve GPIO lib
 GPIO = webiopi.GPIO
 
+# -------------------------------------------------- #
+# Macro definition part                              #
+# -------------------------------------------------- #
+
 # A custom macro which prints out the arg received and return OK
-def myMacro(arg1, arg2, arg3):
-    print("myMacro(%s, %s, %s)" % (arg1, arg2, arg3))
+def myMacroWithArgs(arg1, arg2, arg3):
+    print("myMacroWithArgs(%s, %s, %s)" % (arg1, arg2, arg3))
     return "OK"
+
+# A custom macro without args which return nothing
+def myMacroWithoutArgs():
+    print("myMacroWithoutArgs()")
 
 # Example loop which toggle GPIO 7 each 5 seconds
 def loop():
     GPIO.output(7, not GPIO.input(7))
     time.sleep(5)        
-    
 
-# Instantiate the server on the port 8000, it starts immediately in its own thread
-server = webiopi.Server(port=8000, login="webiopi", password="raspberry")
-# or     webiopi.Server(port=8000, passwdfile="/etc/webiopi/passwd")
-
-# Register the macro so you can call it through the [RESTAPI] or [JAVASCRIPT]
-server.addMacro(myMacro)
+# -------------------------------------------------- #
+# Initialization part                                #
+# -------------------------------------------------- #
 
 # Setup GPIOs
 GPIO.setFunction(1, GPIO.IN)
@@ -32,10 +36,35 @@ GPIO.setFunction(9, GPIO.PWM)
 GPIO.pulseRatio(8, 0.5) # init to 50% duty cycle ratio
 GPIO.pulseAngle(9, 0)   # init to neutral
 
-# run our loop until CTRL-C is pressed
+# -------------------------------------------------- #
+# Main server part                                   #
+# -------------------------------------------------- #
+
+# Instantiate the server on the port 8000, it starts immediately in its own thread
+server = webiopi.Server(port=8000, login="webiopi", password="raspberry")
+# or     webiopi.Server(port=8000, passwdfile="/etc/webiopi/passwd")
+
+# Register the macros so you can call it with Javascript and/or REST API
+server.addMacro(myMacroWithArgs)
+server.addMacro(myMacroWithoutArgs)
+
+
+# -------------------------------------------------- #
+# Loop execution part                                #
+# -------------------------------------------------- #
+
+# Run our loop until CTRL-C is pressed or SIGTERM received
 webiopi.runLoop(loop)
 
-# Stop the server
+# If no specific loop is needed and defined above, just use 
+# webiopi.runLoop()
+# here instead
+
+# -------------------------------------------------- #
+# Termination part                                   #
+# -------------------------------------------------- #
+
+# Cleanly stop the server
 server.stop()
 
 # Reset GPIO functions
