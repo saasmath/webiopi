b58aebd4885b86715f47a9f50f08a87173248446
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-01-24 10:48:12 +0000

    improved serial class

--- python/webiopi/serial.py
+++ python/webiopi/serial.py
@@ -4,48 +4,47 @@ import fcntl
 import struct
 import termios
 
+from .bus import *
+
 TIOCINQ   = hasattr(termios, 'FIONREAD') and termios.FIONREAD or 0x541B
 TIOCM_zero_str = struct.pack('I', 0)
 
-class Serial:
-    def __init__(self, baudrate=9600, port="/dev/ttyAMA0"):
+class Serial(Bus):
+    def __init__(self, baudrate=9600, device="/dev/ttyAMA0"):
         aname = "B%d" % baudrate
         if not hasattr(termios, aname):
             raise Exception("Unsupported baudrate")
-        speed = getattr(termios, aname)
 
-        self.fd = os.open(port, os.O_RDWR | os.O_NOCTTY)# | os.O_NDELAY)
-        
-        if self.fd < 0:
-            raise Exception("Cannot open %s" % port)
-        
+        Bus.__init__(self, [], device, os.O_RDWR | os.O_NOCTTY)
         fcntl.fcntl(self.fd, fcntl.F_SETFL, os.O_NDELAY)
         
         backup  = termios.tcgetattr(self.fd)
         options = termios.tcgetattr(self.fd)
-        
-        options[4] = speed # ispeed
-        options[5] = speed # ospeed
-        
-        # cflags
+        # iflag
+        options[0] = 0
+
+        # oflag
+        options[1] = 0
+
+        # cflag
         options[2] |= (termios.CLOCAL | termios.CREAD)
         options[2] &= ~termios.PARENB
         options[2] &= ~termios.CSTOPB
         options[2] &= ~termios.CSIZE
         options[2] |= termios.CS8
+
+	# lflag
+        options[3] = 0
+
+        speed = getattr(termios, aname)
+        # ispeed
+        options[4] = speed
+        # ospeed
+        options[5] = speed
         
         termios.tcsetattr(self.fd, termios.TCSADRAIN, options)
     
-    def close(self):
-        os.close(self.fd)
-
     def available(self):
         s = fcntl.ioctl(self.fd, TIOCINQ, TIOCM_zero_str)
         return struct.unpack('I',s)[0]
-        
-    def read(self, size=1):
-        return os.read(self.fd, size)
-        
-    def write(self, bytes):
-        os.write(self.fd, bytes)
         
