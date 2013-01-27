d217037f703c0ddcd7cbf57b027fd67b74f79982
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-01-27 20:52:47 +0000

    added device.digital subpackage with PCF8574

---
+++ python/webiopi/devices/digital.py
@@ -0,0 +1,23 @@
+from webiopi.utils import route
+from webiopi.i2c import I2C
+
+class PCF8574(I2C):
+    def __init__(self, addr=0x20):
+        I2C.__init__(self, addr)
+        
+    @route("GET", "%(channel)d/value")
+    def input(self, channel):
+        mask = 1 << channel
+        d = self.readByte()
+        return (d & mask) == mask 
+
+    @route("POST", "%(channel)d/value/%(value)d")
+    def output(self, channel, value):
+        b  = self.readByte()
+        b &= ~(1 << channel)
+        b |= value << channel
+        self.writeBytes(b)
+        return self.input(channel)  
+
+__all__ = [PCF8574]
+
