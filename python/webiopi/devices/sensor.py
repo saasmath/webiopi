b8fa8a8c34445a7c09561100fb921c5a179ed3d4
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-01-30 18:58:03 +0000

    added TMP075 and TMP275

--- python/webiopi/devices/sensor.py
+++ python/webiopi/devices/sensor.py
@@ -2,14 +2,24 @@ from webiopi.utils import route
 from webiopi.i2c import I2C
 
 
-class TMP102(I2C):
-    def __init__(self, addr=0b1001000):
-        I2C.__init__(self, addr, "TMP102")
+class TMPXXX(I2C):
+    def __init__(self, addr=0b1001000, name="TMPXXX"):
+        I2C.__init__(self, addr, name)
         
     @route("GET", "temperature", "%.02f")
     def getTemperature(self):
         d = self.readBytes(2)
         return ((d[0] << 4) | (d[1] >> 4)) *0.0625
 
-__all__ = [TMP102]
+class TMP075(TMPXXX):
+    def __init__(self, addr=0b1001000):
+        TMPXXX.__init__(self, addr, "TMP075")
+
+class TMP102(TMPXXX):
+    def __init__(self, addr=0b1001000):
+        TMPXXX.__init__(self, addr, "TMP102")
+
+class TMP275(TMPXXX):
+    def __init__(self, addr=0b1001000):
+        TMPXXX.__init__(self, addr, "TMP275")
 
