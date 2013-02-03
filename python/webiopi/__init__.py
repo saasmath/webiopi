a014db96d46a9018994bf82bfe49cd95583b2e3e
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-02-03 22:04:18 +0000

    added/updated license

--- python/webiopi/__init__.py
+++ python/webiopi/__init__.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-
-#   Copyright 2012 Eric Ptak - trouch.com
+#   Copyright 2012-2013 Eric Ptak - trouch.com
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
