debae7ec84a2c82c55461d8436bf08a873e0b34e
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-02-05 21:56:02 +0000

    Devices sorted by name then type

--- python/webiopi/rest.py
+++ python/webiopi/rest.py
@@ -101,7 +101,7 @@ class RESTHandler():
             else:
                 device[type] = instance.__str__()
             devices.append(device)
-        return jsonDumps(sorted(devices, key=lambda dev: dev[type]))
+        return jsonDumps(sorted(sorted(devices, key=lambda dev: dev[name]), key=lambda dev: dev[type]))
         
     def addRoute(self, source, destination):
         if source[0] == "/":
