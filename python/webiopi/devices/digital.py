98ec6ee40c00f351794673b2d863b338166d6da0
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-02-14 22:15:50 +0000

    partially refactored GPIOPort and NativeGPIO, added MCP230XX drivers

--- python/webiopi/devices/digital.py
+++ python/webiopi/devices/digital.py
@@ -35,12 +35,18 @@ class Port():
     def getChannelCount(self):
         return self.channelCount
 
-class InputPort(Port):
+class GPIOPort(Port):
     def __init__(self, channelCount):
         Port.__init__(self, channelCount)
     
     def __family__(self):
-        return "Input"
+        return "GPIO"
+    
+    def __getFunction__(self, channel):
+        raise NotImplementedError
+    
+    def __setFunction__(self, channel, func):
+        raise NotImplementedError
     
     def __input__(self, chanel):
         raise NotImplementedError
@@ -48,6 +54,46 @@ class InputPort(Port):
     def __readInteger__(self):
         raise NotImplementedError
     
+    def __output__(self, chanel, value):
+        raise NotImplementedError
+        
+    def __writeInteger__(self, value):
+        raise NotImplementedError
+    
+    def getFunction(self, channel):
+        self.checkChannel(channel)
+        return self.__getFunction__(channel)  
+    
+    @request("GET", "%(channel)d/function")
+    def getFunctionString(self, channel):
+        func = self.getFunction(channel)
+        if func == GPIO.IN:
+            return "IN"
+        elif func == GPIO.OUT:
+            return "OUT"
+        elif func == GPIO.PWM:
+            return "PWM"
+        else:
+            return "UNKNOWN"
+        
+    def setFunction(self, channel, value):
+        self.checkChannel(channel)
+        self.__setFunction__(channel, value)
+        return self.getFunction(channel)
+
+    @request("POST", "%(channel)d/function/%(value)s")
+    def setFunctionString(self, channel, value):
+        value = value.lower()
+        if value == "in":
+            self.setFunction(channel, GPIO.IN)
+        elif value == "out":
+            self.setFunction(channel, GPIO.OUT)
+        elif value == "pwm":
+            self.setFunction(channel, GPIO.PWM)
+        else:
+            raise ValueError("Bad Function")
+        return self.getFunctionString(channel)  
+
     @request("GET", "%(channel)d/value")
     @response("%d")
     def input(self, channel):
@@ -56,10 +102,21 @@ class InputPort(Port):
 
     @request("GET", "*")
     @response(contentType=M_JSON)
-    def readAll(self):
+    def readAll(self, compact=False):
+        if compact:
+            f = "f"
+            v = "v"
+        else:
+            f = "function"
+            v = "value"
+            
         values = {}
         for i in range(self.channelCount):
-            values[i] = int(self.input(i))
+            if compact:
+                func = self.getFunction(i)
+            else:
+                func = self.getFunctionString(i)
+            values[i] = {f: func, v: int(self.input(i))}
         return jsonDumps(values)
 
     @request("GET", "integer")
@@ -67,22 +124,6 @@ class InputPort(Port):
     def readInteger(self):
         return self.__readInteger__()
     
-class OutputPort(Port):
-    def __init__(self, channelCount):
-        Port.__init__(self, channelCount)
-    
-    def __family__(self):
-        return "Output"
-    
-    def __output__(self, chanel, value):
-        raise NotImplementedError
-        
-    def __readInteger__(self):
-        raise NotImplementedError
-    
-    def __writeInteger__(self, value):
-        raise NotImplementedError
-
     @request("POST", "%(channel)d/value/%(value)d")
     @response("%d")
     def output(self, channel, value):
@@ -97,16 +138,9 @@ class OutputPort(Port):
         self.__writeInteger__(value)
         return self.readInteger()
     
-class GPIOPort(InputPort, OutputPort):
-    def __init__(self, channelCount):
-        InputPort.__init__(self, channelCount)
-        OutputPort.__init__(self, channelCount)
-        
-    def __family__(self):
-        return "GPIOPort"
-    
-        
 class PCF8574(I2C, GPIOPort):
+    FUNCTIONS = [GPIO.IN for i in range(8)]
+    
     def __init__(self, slave=0x20):
         slave = toint(slave)
         if slave in range(0x20, 0x28):
@@ -118,6 +152,16 @@ class PCF8574(I2C, GPIOPort):
         
         I2C.__init__(self, slave, name)
         GPIOPort.__init__(self, 8)
+        self.writeInteger(0xFF)
+        self.readInteger()
+        
+    def __getFunction__(self, channel):
+        return self.FUNCTIONS[channel]
+    
+    def __setFunction__(self, channel, value):
+        if not value in [GPIO.IN, GPIO.OUT]:
+            raise ValueError("Requested function not supported")
+        self.FUNCTIONS[channel] = value
         
     def __input__(self, channel):
         mask = 1 << channel
@@ -142,3 +186,87 @@ class PCF8574(I2C, GPIOPort):
 class PCF8574A(PCF8574):
     def __init__(self, slave=0x38):
         PCF8574.__init__(self, slave)
+        
+class MCP230XX(GPIOPort, I2C):
+    IODIR   = 0x00
+    IPOL    = 0x01
+    GPINTEN = 0x02
+    DEFVAL  = 0x03
+    INTCON  = 0x04
+    IOCON   = 0x05
+    GPPU    = 0x06
+    INTF    = 0x07
+    INTCAP  = 0x08
+    GPIO    = 0x09
+    OLAT    = 0x0A
+    
+    BANK0_IOCON = 0x0A
+    
+    def __init__(self, slave, channelCount, name="MCP230XX"):
+        I2C.__init__(self, slave, name)
+        GPIOPort.__init__(self, channelCount)
+        self.banks = int(channelCount / 8)
+        
+    def getChannel(self, register, channel):
+        self.checkChannel(channel)
+        addr = register * self.banks + int(channel / 8) 
+        mask = 1 << (channel % 8)
+        return (addr, mask)
+    
+    def __input__(self, channel):
+        (addr, mask) = self.getChannel(self.GPIO, channel) 
+        d = self.readRegister(addr)
+        return (d & mask) == mask
+
+    def __output__(self, channel, value):
+        (addr, mask) = self.getChannel(self.GPIO, channel) 
+        d = self.readRegister(addr)
+        if value:
+            d |= mask
+        else:
+            d &= ~mask
+        self.writeRegister(addr, d)
+        
+    def __getFunction__(self, channel):
+        (addr, mask) = self.getChannel(self.IODIR, channel) 
+        d = self.readRegister(addr)
+        return GPIO.IN if (d & mask) == mask else GPIO.OUT
+        
+    def __setFunction__(self, channel, value):
+        if not value in [GPIO.IN, GPIO.OUT]:
+            raise ValueError("Requested function not supported")
+
+        (addr, mask) = self.getChannel(self.IODIR, channel) 
+        d = self.readRegister(addr)
+        if value == GPIO.IN:
+            d |= mask
+        else:
+            d &= ~mask
+        self.writeRegister(addr, d)
+
+    def __readInteger__(self):
+        value = 0
+        for i in range(self.banks):
+            value |= self.readRegister(self.banks*self.GPIO+i) << 8*i
+        return value
+    
+    def __writeInteger__(self, value):
+        for i in range(self.banks):
+            self.writeRegister(self.banks*self.GPIO+i,  (value >> 8*i) & 0xFF)
+
+class MCP23008(MCP230XX):
+    def __init__(self, slave=0x20):
+        MCP230XX.__init__(self, slave, 8, "MCP23008")
+
+class MCP23009(MCP230XX):
+    def __init__(self, slave=0x20):
+        MCP230XX.__init__(self, slave, 8, "MCP23009")
+
+class MCP23017(MCP230XX):
+    def __init__(self, slave=0x20):
+        MCP230XX.__init__(self, slave, 16, "MCP23017")
+
+class MCP23018(MCP230XX):
+    def __init__(self, slave=0x20):
+        MCP230XX.__init__(self, slave, 16, "MCP23018")
+
--- python/webiopi/devices/gpio.py
+++ python/webiopi/devices/gpio.py
@@ -5,56 +5,87 @@ from webiopi.devices.digital import *
 class NativeGPIO(GPIOPort):
     def __init__(self):
         GPIOPort.__init__(self, 54)
-        self.export = []
+        self.export = range(54)
         self.post_value = True
         self.post_function = True
         
     def __str__(self):
         return "GPIO"
     
+    def checkChannelExported(self, channel):
+        if not channel in self.export:
+            raise GPIO.InvalidChannelException("Channel %d is not allowed" % channel)
+        
     def checkPostingFunctionAllowed(self):
         if not self.post_function:
-            raise ValueError("POSTing function not allowed")
+            raise ValueError("POSTing function to native GPIO not allowed")
     
     def checkPostingValueAllowed(self):
         if not self.post_value:
-            raise ValueError("POSTing value not allowed")
+            raise ValueError("POSTing value to native GPIO not allowed")
     
     def __input__(self, channel):
+        self.checkChannelExported(channel)
         return GPIO.input(channel)
     
     def __output__(self, channel, value):
+        self.checkChannelExported(channel)
         self.checkPostingValueAllowed()
         GPIO.output(channel, value)
 
-    @request("GET", "%(channel)d/function")
-    def getFunctionString(self, channel):
-        self.checkChannel(channel)
-        return GPIO.getFunctionString(channel)
+    def __getFunction__(self, channel):
+        self.checkChannelExported(channel)
+        return GPIO.getFunction(channel)
     
-    @request("POST", "%(channel)d/function/%(value)s")
-    def setFunction(self, channel, value):
+    def __setFunction__(self, channel, value):
+        self.checkChannelExported(channel)
         self.checkPostingFunctionAllowed()
-        self.checkChannel(channel)
-        value = value.lower()
-        if value == "in":
-            GPIO.setFunction(channel, GPIO.IN)
-        elif value == "out":
-            GPIO.setFunction(channel, GPIO.OUT)
-        elif value == "pwm":
-            GPIO.setFunction(channel, GPIO.PWM)
+        GPIO.setFunction(channel, value)
+        
+    def __readInteger__(self):
+        value = 0
+        for i in self.export:
+            value |= GPIO.input(i) << i
+        return value 
+            
+    def __writeInteger__(self, value):
+        if len(self.export) < 54:
+            for i in self.export:
+                if GPIO.getFunction(i) == GPIO.OUT:
+                    GPIO.output(i, (value >> i) & 1)
         else:
-            raise ValueError("Bad Function")
-        return GPIO.getFunctionString(channel)
+            raise Exception("Please limit exported GPIO to write integers")
+            
+    @request("GET", "*")
+    @response(contentType=M_JSON)
+    def readAll(self, compact=False):
+        if compact:
+            f = "f"
+            v = "v"
+        else:
+            f = "function"
+            v = "value"
+            
+        values = {}
+        for i in self.export:
+            if compact:
+                func = GPIO.getFunction(i)
+            else:
+                func = GPIO.getFunctionString(i)
+            values[i] = {f: func, v: int(GPIO.input(i))}
+        return jsonDumps(values)
+
     
     @request("GET", "%(channel)d/pulse", "%s")
     def getPulse(self, channel):
+        self.checkChannelExported(channel)
         self.checkChannel(channel)
         return GPIO.getPulse(channel)
     
     @request("POST", "%(channel)d/sequence/%(args)s")
     @response("%d")
     def outputSequence(self, channel, args):
+        self.checkChannelExported(channel)
         self.checkPostingValueAllowed()
         self.checkChannel(channel)
         (period, sequence) = args.split(",")
@@ -64,6 +95,7 @@ class NativeGPIO(GPIOPort):
         
     @request("POST", "%(channel)d/pulse/")
     def pulse(self, channel):
+        self.checkChannelExported(channel)
         self.checkPostingValueAllowed()
         self.checkChannel(channel)
         GPIO.pulse(channel)
@@ -71,6 +103,7 @@ class NativeGPIO(GPIOPort):
         
     @request("POST", "%(channel)d/pulseRatio/%(value)f")
     def pulseRatio(self, channel, value):
+        self.checkChannelExported(channel)
         self.checkPostingValueAllowed()
         self.checkChannel(channel)
         GPIO.pulseRatio(channel, value)
@@ -78,6 +111,7 @@ class NativeGPIO(GPIOPort):
         
     @request("POST", "%(channel)d/pulseAngle/%(value)f")
     def pulseAngle(self, channel, value):
+        self.checkChannelExported(channel)
         self.checkPostingValueAllowed()
         self.checkChannel(channel)
         GPIO.pulseAngle(channel, value)
