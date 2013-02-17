9bbb96c18b0f511f81a6c37e65ef8c1fe98fed95
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-02-17 16:00:05 +0000

    updated jquery, added jquery mobile. touch down/up handling.

--- htdocs/webiopi.js
+++ htdocs/webiopi.js
@@ -29,7 +29,23 @@ function webiopi() {
 	return w();
 }
 
+function isMobileUserAgent(a) {
+	if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))
+			return true
+}
+
+var _isMobile = undefined;
+function isMobile() {
+	if (_isMobile == undefined) {
+		_isMobile = ((navigator.userAgent != undefined && isMobileUserAgent(navigator.userAgent))
+				|| (navigator.vendor != undefined && isMobileUserAgent(navigator.vendor)) 
+				|| (window.opera != undefined && isMobileUserAgent(window.opera)))
+	}
+	return _isMobile
+}
+
 function WebIOPi() {
+	this.readyCallback = null;
 	this.context = "/";
 	this.GPIO = Array(54);
 	this.PINS = Array(27);
@@ -59,10 +75,8 @@ function WebIOPi() {
 	}
 
 	// get context
-	var scripts = document.getElementsByTagName("script");
-	//var styles = document.getElementsByTagName("link");
-
 	var reg = new RegExp("http://" + window.location.host + "(.*)webiopi.js");
+	var scripts = document.getElementsByTagName("script");
 	for(var i = 0; i < scripts.length; i++) {
 		var res = reg.exec(scripts[i].src);
 		if (res && (res.length > 1)) {
@@ -74,25 +88,34 @@ function WebIOPi() {
 
 	var head = document.getElementsByTagName('head')[0];
 
-	var style = document.createElement('link');
-	style.rel = "stylesheet";
-	style.type = 'text/css';
-	style.href = '/webiopi.css';
-	head.insertBefore(style, scripts[0]);
-	
 	var jquery = document.createElement('script');
 	jquery.type = 'text/javascript';
 	jquery.src = '/jquery.js';
-	jquery.onload = function() {
-		w().init();
-	};
-	jquery.onreadystatechange = function() {
-		if (this.readyState == 'complete') {
+	if (!isMobile()) {
+		jquery.onload = function() {
 			w().init();
-		}
-	};
-	head.insertBefore(jquery, scripts[0]);
+		};
+	}
+//	jquery.onreadystatechange = function() {
+//		alert("ready state change");
+//		if (this.readyState == 'complete') {
+//			alert("complete");
+//			w().init();
+//		}
+//	};
+	head.appendChild(jquery);
 	
+	if (isMobile()) {
+		console.log("load jquery mobile");
+		var mobile = document.createElement('script');
+		mobile.type = 'text/javascript';
+		mobile.src = '/jquery-mobile.js';
+		mobile.onload = function() {
+			w().initMobile()
+		};
+		head.appendChild(mobile);
+	}
+
 	// GA
 	_gaq.push(['_setAccount', 'UA-33979593-2']);
 	_gaq.push(['_trackPageview']);
@@ -101,7 +124,21 @@ function WebIOPi() {
 	ga.type = 'text/javascript';
 	ga.async = false;
 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	head.insertBefore(ga, scripts[0]);
+	head.appendChild(ga);
+	
+	var style = document.createElement('link');
+	style.rel = "stylesheet";
+	style.type = 'text/css';
+	style.href = '/webiopi.css';
+	head.appendChild(style);
+
+	if (isMobile()) {
+		var style = document.createElement('link');
+		style.rel = "stylesheet";
+		style.type = 'text/css';
+		style.href = '/jquery-mobile.css';
+		head.appendChild(style);
+	}
 	
 	// init ALTs
 	this.addALT(this.ALT.I2C, 0, "SDA");
@@ -147,14 +184,17 @@ WebIOPi.prototype.init = function() {
 			
 			w().map(i+1, type, label);
 		}
-		if (w().readyCallback != undefined) {
+		if (w().readyCallback != null) {
 			w().readyCallback();
 		}
-		w().updateUI();
+
 		w().checkVersion();
 	});
 }
 
+WebIOPi.prototype.initMobile = function() {
+	webiopi().init();
+}
 
 WebIOPi.prototype.ready = function (cb) {
 	w().readyCallback = cb;
@@ -211,7 +251,7 @@ WebIOPi.prototype.updateALT = function (alt, enable) {
 	alt.enabled = enable;
 }
 
-WebIOPi.prototype.updateUI = function () {
+WebIOPi.prototype.refreshGPIO = function (repeat) {
 	$.getJSON(w().context + "*", function(data) {
 		w().updateALT(w().ALT.I2C, data["I2C"]);
 		w().updateALT(w().ALT.SPI, data["SPI"]);
@@ -231,7 +271,9 @@ WebIOPi.prototype.updateUI = function () {
 	    	
 		});
 	});
-	setTimeout(w().updateUI, 1000);
+	if (repeat === true) {
+		setTimeout(function(){w().refreshGPIO(repeat)}, 1000);
+	}
 }
 
 
@@ -280,7 +322,7 @@ WebIOPi.prototype.setValue = function (gpio, value, callback) {
 		});
 	}
 	else {
-		console.log(w().GPIO[gpio].func);
+		//console.log(w().GPIO[gpio].func);
 	}
 }
 
@@ -385,8 +427,14 @@ WebIOPi.prototype.createButton = function (id, label, callback, callbackUp) {
 		button.bind("click", callback);
 	}
 	else if ((callback != undefined) && (callbackUp != undefined)) {
-		button.bind("mousedown", callback);
-		button.bind("mouseup", callbackUp);
+		if (isMobile()) {
+			button.bind("vmousedown", callback);
+			button.bind("vmouseup", callbackUp);
+		}
+		else {
+			button.bind("mousedown", callback);
+			button.bind("mouseup", callbackUp);
+		}
 	}
 	return button;
 }
