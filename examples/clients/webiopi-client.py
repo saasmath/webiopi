88afc10e09b7c5d28e960084c7095a76d6e4ce78
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2013-03-16 21:42:44 +0000

    added credential usage

--- examples/clients/webiopi-client.py
+++ examples/clients/webiopi-client.py
@@ -2,7 +2,11 @@ from webiopi import runLoop
 from webiopi.clients import *
 from time import sleep
 
-client = PiMixedClient("192.168.1.234")
+client = PiHttpClient("192.168.1.234")
+#client = PiMixedClient("192.168.1.234")
+#client = PiCoapClient("192.168.1.234")
+
+#client.setCredentials("webiopi", "raspberry")
 
 gpio = NativeGPIO(client)
 gpio.setFunction(25, "out")
