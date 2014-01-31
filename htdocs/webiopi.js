28cb96757dc3949bfbc1eb99a54695e3a6b5c42b
trouch@trouch.com <trouch@trouch.com@728a0143-98a1-36eb-31b9-d59f1c09b120>
2014-01-31 20:02:27 +0000

    fixed issue #73 - switched mobile to touch mobile detection and changed touch/mouse handling

--- htdocs/webiopi.js
+++ htdocs/webiopi.js
@@ -29,6 +29,7 @@ function webiopi() {
 	return w();
 }
 
+/*
 function isMobileUserAgent(a) {
 	if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))
 			return true
@@ -43,6 +44,28 @@ function isMobile() {
 	}
 	return _isMobile
 }
+*/
+
+var deviceAgent = navigator.userAgent.toLowerCase();
+var isTouchDevice = false;
+var downEvent = "mousedown";
+var upEvent = "mouseup";
+if (deviceAgent.match(/(iphone|ipod|ipad)/) ||
+		deviceAgent.match(/(android)/)  || 
+		deviceAgent.match(/(iemobile)/) ||
+		deviceAgent.match(/iphone/i) ||
+		deviceAgent.match(/ipad/i) || 
+		deviceAgent.match(/ipod/i) || 
+		deviceAgent.match(/blackberry/i) ||
+		deviceAgent.match(/bada/i) ||
+	"ontouchstart" in document.documentElement
+	) {
+        isTouchDevice = true;
+		downEvent = "touchstart";
+		upEvent = "touchend";
+}
+
+
 
 function WebIOPi() {
 	this.readyCallback = null;
@@ -91,13 +114,14 @@ function WebIOPi() {
 	var jquery = document.createElement('script');
 	jquery.type = 'text/javascript';
 	jquery.src = '/jquery.js';
-	if (!isMobile()) {
+//	if (!isMobile()) {
 		jquery.onload = function() {
 			w().init();
 		};
-	}
+//	}
 	head.appendChild(jquery);
 
+/*
 	if (isMobile()) {
 		console.log("load jquery mobile");
 		var mobile = document.createElement('script');
@@ -108,6 +132,7 @@ function WebIOPi() {
 		};
 		head.appendChild(mobile);
 	}
+*/
 
 	// GA
 	_gaq.push(['_setAccount', 'UA-33979593-2']);
@@ -125,6 +150,7 @@ function WebIOPi() {
 	style.href = '/webiopi.css';
 	head.appendChild(style);
 
+/*
 	if (isMobile()) {
 		var style = document.createElement('link');
 		style.rel = "stylesheet";
@@ -132,6 +158,7 @@ function WebIOPi() {
 		style.href = '/jquery-mobile.css';
 		head.appendChild(style);
 	}
+*/
 	
 	// init ALTs
 	this.addALT(this.ALT.I2C, 0, "SDA");
@@ -185,9 +212,11 @@ WebIOPi.prototype.init = function() {
 	});
 }
 
+/*
 WebIOPi.prototype.initMobile = function() {
 	webiopi().init();
 }
+*/
 
 WebIOPi.prototype.ready = function (cb) {
 	w().readyCallback = cb;
@@ -420,25 +449,18 @@ WebIOPi.prototype.createButton = function (id, label, callback, callbackUp) {
 	var button = $('<button type="button" class="Default">');
 	button.attr("id", id);
 	button.text(label);
-	if ((callback != undefined) && (callbackUp == undefined)) {
-		button.bind("click", callback);
+	if (callback != undefined) {
+		button.bind(downEvent, callback);
 	}
-	else if ((callback != undefined) && (callbackUp != undefined)) {
-		if (isMobile()) {
-			button.bind("vmousedown", callback);
-			button.bind("vmouseup", callbackUp);
-		}
-		else {
-			button.bind("mousedown", callback);
-			button.bind("mouseup", callbackUp);
-		}
+	if (callbackUp != undefined) {
+		button.bind(upEvent, callbackUp);
 	}
 	return button;
 }
 
 WebIOPi.prototype.createGPIOButton = function (gpio, label) {
 	var button = w().createButton("gpio" + gpio, label);
-	button.bind("click", function(event) {
+	button.bind(downEvent, function(event) {
 		w().toggleValue(gpio);
 	});
 	return button;
@@ -447,7 +469,7 @@ WebIOPi.prototype.createGPIOButton = function (gpio, label) {
 WebIOPi.prototype.createFunctionButton = function (gpio) {
 	var button = w().createButton("function" + gpio, " ");
 	button.attr("class", "FunctionBasic");
-	button.bind("click", function(event) {
+	button.bind(downEvent, function(event) {
 		w().toggleFunction(gpio);
 	});
 	return button;
@@ -455,7 +477,7 @@ WebIOPi.prototype.createFunctionButton = function (gpio) {
 
 WebIOPi.prototype.createPulseButton = function (id, label, gpio) {
     var button = webiopi().createButton(id, label);
-    button.bind("click", function(event) {
+    button.bind(downEvent, function(event) {
         webiopi().pulse(gpio);
     });
     return button;
@@ -463,7 +485,7 @@ WebIOPi.prototype.createPulseButton = function (id, label, gpio) {
 
 WebIOPi.prototype.createMacroButton = function (id, label, macro, args) {
     var button = webiopi().createButton(id, label);
-    button.bind("click", function(event) {
+    button.bind(downEvent, function(event) {
         webiopi().callMacro(macro, args);
     });
     return button;
@@ -471,7 +493,7 @@ WebIOPi.prototype.createMacroButton = function (id, label, macro, args) {
 
 WebIOPi.prototype.createSequenceButton = function (id, label, gpio, period, sequence) {
     var button = webiopi().createButton(id, label);
-    button.bind("click", function(event) {
+    button.bind(downEvent, function(event) {
         webiopi().outputSequence(gpio, period, sequence);
     });
     return button;
